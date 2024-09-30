c60b5c1cece4e640ba1a4737e80c9f75dd3cfe8d
BenDXC <benlate0@gmail.com>
2024-09-30 16:15:26 +0100

    First Commit: Prerequisties

---
+++ passwordmanager/src/main/java/com/passwordmanager/passwordmanager/PasswordManager.java
@@ -0,0 +1,12 @@
+package com.passwordmanager.passwordmanager;
+
+public class PasswordManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
